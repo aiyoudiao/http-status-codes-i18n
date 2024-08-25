c39e4df6bf4bbc5417a1875d15d87ad1898207a7
aiyoudiao <newdiao@163.com>
2024-08-26 03:13:45 +0800

    🐛 fix: fix publish.sh

--- publish.sh
+++ publish.sh
@@ -3,8 +3,11 @@
 # 确保脚本具有执行权限
 chmod +x "$0"
 
+# 登录 npm
+npm login
+
 # 指定要发布的项目名称列表
-projects=("i18n-en" "i18n-zh" "project3")
+projects=("i18n-en" "i18n-zh")
 
 # 获取当前脚本所在目录
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
@@ -16,8 +19,10 @@ for project in "${projects[@]}"; do
 
   # 检查 package.json 文件是否存在
   if [ -f "$PACKAGE_JSON" ]; then
-    echo "Publishing $project..."
+    echo "Fixing package.json for $project..."
     cd "$PROJECT_DIR" || exit
+    npm pkg fix
+    echo "Publishing $project..."
     npm publish --access public
     cd "$SCRIPT_DIR" || exit
     echo "$project published successfully."
